19b0b7395297be4dfb4bf8431e08b6afb70dd739
Alexis CASSION <acassion@dotscreen.com>
2024-01-08 13:18:33 +0100

    initial commit

---
+++ app/src/main/java/com/example/openweatherapitest/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.openweatherapitest
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        //todo: implement activity
+        super.onCreate(savedInstanceState)
+    }
+}
+
