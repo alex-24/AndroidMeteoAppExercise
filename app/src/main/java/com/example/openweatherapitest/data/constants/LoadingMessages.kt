5c155d11dbda2e23f5cfc887ad50c42bed12730c
Alexis CASSION <acassion@dotscreen.com>
2024-01-10 23:00:01 +0100

    delete uneeded file

--- app/src/main/java/com/example/openweatherapitest/data/constants/LoadingMessages.kt
+++
@@ -1,9 +0,0 @@
-package com.example.openweatherapitest.data.constants
-
-class LoadingMessages {
-    val list = listOf(
-        "Nous téléchargeons les données…",
-        "C’est presque fini…",
-        "Plus que quelques secondes avant d’avoir le résultat…",
-    )
-}
