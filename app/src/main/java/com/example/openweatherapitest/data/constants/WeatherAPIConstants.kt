9a804d83dc49b693d5c73bfc51ce5a2be0adb70f
Alexis CASSION <acassion@dotscreen.com>
2024-01-11 01:09:44 +0100

    added pictogram

--- app/src/main/java/com/example/openweatherapitest/data/constants/WeatherAPIConstants.kt
+++ app/src/main/java/com/example/openweatherapitest/data/constants/WeatherAPIConstants.kt
@@ -3,6 +3,7 @@ package com.example.openweatherapitest.data.constants
 class WeatherAPIConstants {
     companion object {
         val BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
+        val PICTOGRAM_BASE_URL = "https://openweathermap.org/img/wn"
         val key = ""
     }
 }
